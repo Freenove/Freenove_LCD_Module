4f03df728d3efd7975f0140cf97b3181e308ec87
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-03-17 15:48:40 +0800

    Update

--- Freenove_LCD_Module_for_Arduino/Sketches/Sketch_1.1_Display_the_string_on_LCD1602/Sketch_1.1_Display_the_string_on_LCD1602.ino
+++ Freenove_LCD_Module_for_Arduino/Sketches/Sketch_1.1_Display_the_string_on_LCD1602/Sketch_1.1_Display_the_string_on_LCD1602.ino
@@ -5,20 +5,32 @@
   Modification: 2022/06/28
 **********************************************************************/
 #include <LiquidCrystal_I2C.h>
-/*
-* note:If lcd1602 uses PCF8574T, IIC's address is 0x27,
-* or lcd1602 uses PCF8574AT, IIC's address is 0x3F.
-*/
-LiquidCrystal_I2C lcd(0x27,16,2);
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
-  lcd.init(); // LCD driver initialization
-  lcd.backlight(); // Open the backlight
-  lcd.setCursor(0,0); // Move the cursor to row 0, column 0
-  lcd.print("hello world"); // The print content is displayed on the LCD
+  if (!i2CAddrTest(0x27)) {
+    lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+  }
+  lcd.init();                // initialize the lcd
+  lcd.backlight();           // Turn on backlight
+  lcd.print("hello, world!");// Print a message to the LCD
 }
+
 void loop() {
-  lcd.setCursor(0,1); // Move the cursor to row 1, column 0
-  lcd.print("Counter:"); // The count is displayed every second
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);// set the cursor to column 0, line 1
+  // print the number of seconds since reset:
+  lcd.print("Counter:");
   lcd.print(millis() / 1000);
-  delay(1000);
+}
+
+bool i2CAddrTest(uint8_t addr) {
+  Wire.begin();
+  Wire.beginTransmission(addr);
+  if (Wire.endTransmission() == 0) {
+    return true;
+  }
+  return false;
 }
