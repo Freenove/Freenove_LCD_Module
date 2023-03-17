f868c874f7bb4e5e77df3f81c3375961ba44d2c9
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-03-17 15:45:54 +0800

    Update Sketch_1.1_Display_the_string_on_LCD1602.ino

--- Freenove_LCD_Module_for_Raspberry_Pi_Pico/C/Sketches/Sketch_1.1_Display_the_string_on_LCD1602/Sketch_1.1_Display_the_string_on_LCD1602.ino
+++ Freenove_LCD_Module_for_Raspberry_Pi_Pico/C/Sketches/Sketch_1.1_Display_the_string_on_LCD1602/Sketch_1.1_Display_the_string_on_LCD1602.ino
@@ -5,20 +5,31 @@
   Modification: 2022/06/28
 **********************************************************************/
 #include <LiquidCrystal_I2C.h>
-/*
-* note:If lcd1602 uses PCF8574T, IIC's address is 0x27,
-* or lcd1602 uses PCF8574AT, IIC's address is 0x3F.
-*/
-LiquidCrystal_I2C lcd(0x27,16,2);
+
+LiquidCrystal_I2C lcd(0x27,16,2); 
+
 void setup() {
-  lcd.init(); // LCD driver initialization
-  lcd.backlight(); // Open the backlight
-  lcd.setCursor(0,0); // Move the cursor to row 0, column 0
-  lcd.print("hello world"); // The print content is displayed on the LCD
+  if (!i2CAddrTest(0x27)) {
+    lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+  }
+  lcd.init();                     // LCD driver initialization
+  lcd.backlight();                // Open the backlight
+  lcd.setCursor(0,0);             // Move the cursor to row 0, column 0
+  lcd.print("hello world");       // The print content is displayed on the LCD
 }
+
 void loop() {
-  lcd.setCursor(0,1); // Move the cursor to row 1, column 0
-  lcd.print("Counter:"); // The count is displayed every second
+  lcd.setCursor(0,1);             // Move the cursor to row 1, column 0
+  lcd.print("Counter:");          // The count is displayed every second
   lcd.print(millis() / 1000);
   delay(1000);
+}
+
+bool i2CAddrTest(uint8_t addr) {
+  Wire.begin();
+  Wire.beginTransmission(addr);
+  if (Wire.endTransmission() == 0) {
+    return true;
+  }
+  return false;
 }
